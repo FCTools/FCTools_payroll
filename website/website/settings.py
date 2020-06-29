db469ea0c901dbd9163cfd7f39a6578bce98202b
German Yakimov <german13yakimov@gmail.com>
2020-06-29 20:52:05 +0700

    Change static url to full.

--- website/website/settings.py
+++ website/website/settings.py
@@ -165,7 +165,7 @@ if DEBUG:
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_URL = '/media/'
+MEDIA_URL = os.path.join(BASE_DIR, 'media/')
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
