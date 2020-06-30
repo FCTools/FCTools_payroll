ff2070eb1c801d94043e5a30bd9ba8e1dfc19fd9
German Yakimov <german13yakimov@gmail.com>
2020-06-30 22:06:48 +0700

    Add list_display_links param to models in admin.

--- website/fctools_salary/admin.py
+++ website/fctools_salary/admin.py
@@ -8,16 +8,19 @@ from .models import User, Offer, TrafficSource, Test, Campaign, PercentDependenc
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'login', 'is_lead', 'group')
+    list_display_links = ('id', 'login')
 
 
 @admin.register(Offer)
 class OfferAdmin(admin.ModelAdmin):
-    list_display = ('id', 'geo', 'name', 'group', 'network_name')
+    list_display = ('id', 'name', 'geo', 'group', 'network_name')
+    list_display_links = ('id', 'name')
 
 
 @admin.register(TrafficSource)
 class TrafficSourceAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'user')
+    list_display_links = ('id', 'name')
 
 
 @admin.register(Test)
@@ -34,6 +37,7 @@ class TestAdmin(admin.ModelAdmin):
 @admin.register(Campaign)
 class CampaignAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'traffic_group', 'ts_id', 'cost_rounded', 'revenue_rounded', 'profit_colored', 'user')
+    list_display_links = ('id', 'name')
 
 
 @admin.register(PercentDependency)
