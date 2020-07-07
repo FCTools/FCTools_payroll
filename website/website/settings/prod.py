ec9ea52d4591dac81f47235d9953565caa3f26eb
German Yakimov <german13yakimov@gmail.com>
2020-07-07 14:19:36 +0700

    Add encoding option for database in prod settings.

--- website/website/settings/prod.py
+++ website/website/settings/prod.py
@@ -20,6 +20,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
         'PORT': os.getenv('DATABASE_PORT'),
+        'OPTIONS': {
+                    'charset': 'latin1',
+                    'use_unicode': True, },
     }
 }
 
