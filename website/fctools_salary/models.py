d85ee3efddb0523c52d5d7dc20b4a61d3570aee0
German Yakimov <german13yakimov@gmail.com>
2020-07-23 17:59:54 +0700

    Create models.py for fctools_salary and import all fctools_salary models to models.py

---
+++ website/fctools_salary/models.py
@@ -0,0 +1,7 @@
+from fctools_salary.domains.accounts.user import User
+from fctools_salary.domains.accounts.test import Test
+from fctools_salary.domains.accounts.percent_dependency import PercentDependency
+
+from fctools_salary.domains.tracker.campaign import Campaign
+from fctools_salary.domains.tracker.offer import Offer
+from fctools_salary.domains.tracker.traffic_source import TrafficSource
