262f5d01229552d87ba92fe4ced126750b7e8b15
German Yakimov <german13yakimov@gmail.com>
2020-07-25 11:48:51 +0700

    Add base view with exceptions handling, fix bug with authorization (redirecting to wrong page).

--- website/fctools_salary/views.py
+++ website/fctools_salary/views.py
@@ -1,7 +1,11 @@
+import functools
 import os
+import traceback
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView as DJLogoutView
+from django.db import transaction
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
 from fctools_salary.services.binom.update import update_basic_info
@@ -9,8 +13,59 @@ from fctools_salary.services.engine.engine import calculate_user_salary
 from .forms import ReportInfoForm
 
 
+def _return(json_object, status_code=200):
+    """
+    :param json_object: json-object with error message and traceback
+    :param status_code: response status code
+    :return: Readable json response with error message and traceback (support cyrillic symbols)
+    """
+
+    return JsonResponse(json_object, status=status_code, safe=not isinstance(json_object, list),
+                        json_dumps_params={'ensure_ascii': False, 'indent': 4})
+
+
+def error_response(exception):
+    """
+    Form json-object with error message and traceback.
+
+    :param exception: exception
+    :return: _return-method, that returns json response with json-object with error and traceback
+    """
+
+    result = {"error_message": str(exception), "traceback": traceback.format_exc()}
+    return _return(result, status_code=500)
+
+
+def base_view(view):
+    """
+    Base view with all exceptions handling.
+
+    :param view: view to decorate
+    :return: decorated view
+    """
+
+    @functools.wraps(view)
+    def inner(request, *args, **kwargs):
+        try:
+            with transaction.atomic():
+                return view(request, *args, **kwargs)
+        except Exception as exception:
+            return error_response(exception)
+
+    return inner
+
+
+@base_view
 @login_required(login_url="login/")
 def base_menu(request):
+    """
+    Base menu, contains two buttons: edit db and count salary.
+    Login required.
+
+    :param request: request
+    :return: main menu page
+    """
+
     template = os.path.join("fctools_salary", "menu.html")
     user = request.user.username
 
@@ -19,6 +74,7 @@ def base_menu(request):
     return render(request, template, context)
 
 
+@base_view
 @login_required(login_url="login/")
 def count_view(request):
     if request.method == "POST":
@@ -68,7 +124,8 @@ def count_view(request):
         return render(request, os.path.join("fctools_salary", "count.html"), {"form": form})
 
 
-@login_required(login_url="login/")
+@base_view
+@login_required(login_url="/login/")
 def update_db(request):
     update_basic_info()
 
