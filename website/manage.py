e620420b2967b472649efe23f5b938c54c8253b6
German Yakimov <german13yakimov@gmail.com>
2020-08-20 11:58:05 +0700

    Delete useless requirement (broken psycopg2==2.8.5), change default settings module (website.settings.dev).

--- website/manage.py
+++ website/manage.py
@@ -13,7 +13,7 @@ sys.path.append("..")
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.dev")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
