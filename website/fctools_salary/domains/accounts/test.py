49b36d29c786be6f96c86c5a6a43daaf6e8a8ec1
German Yakimov <german13yakimov@gmail.com>
2020-08-21 17:55:59 +0700

    Alter field adding_time for test (set auto_now_add to True, remove default).

--- website/fctools_salary/domains/accounts/test.py
+++ website/fctools_salary/domains/accounts/test.py
@@ -70,7 +70,7 @@ class Test(models.Model):
     """
     one_budget_for_all_geo = models.BooleanField(verbose_name="One budget for all geo", default=False)
 
-    adding_time = models.DateField(default=datetime.utcnow().date(), verbose_name="Adding time", null=False, blank=False,)
+    adding_time = models.DateField(auto_now_add=True, verbose_name="Adding time", null=False, blank=False,)
 
     lifetime = models.PositiveIntegerField(verbose_name="Test lifetime (days)", default=30, null=False, blank=False,)
 
